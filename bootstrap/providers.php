374fe9af9be3260caf40e4da1f5cc42b48956360
Yishaan <yishaan@Yishaans-MacBook-Air.local>
2026-02-20 18:09:10 +0530

    Remove ArtisanServiceProvider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,7 +3,6 @@
 return [
     Illuminate\Auth\AuthServiceProvider::class,
     Illuminate\Database\DatabaseServiceProvider::class,
-    Illuminate\Foundation\Providers\ArtisanServiceProvider::class,
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
 ];
