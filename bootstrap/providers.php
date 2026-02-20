1e83b0bd49dcd9ca87c488334287daa8611e13e8
Yishaan <yishaan@Yishaans-MacBook-Air.local>
2026-02-20 18:27:56 +0530

    Add ViewServiceProvider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,7 @@
 return [
     Illuminate\Auth\AuthServiceProvider::class,
     Illuminate\Database\DatabaseServiceProvider::class,
+    Illuminate\View\ViewServiceProvider::class,
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
 ];
