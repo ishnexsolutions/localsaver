309094393b0ca6b775d2d1126e6db0611d81ca9f
Yishaan <yishaan@Yishaans-MacBook-Air.local>
2026-02-20 18:00:30 +0530

    Fix files binding for Sanctum

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,7 +8,7 @@ class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        //
+        $this->app->singleton('files', fn () => new \Illuminate\Filesystem\Filesystem);
     }
 
     public function boot(): void
