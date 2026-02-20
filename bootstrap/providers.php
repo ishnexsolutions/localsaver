92b6bac75642013aad719ced001fd4d05444f619
Yishaan <yishaan@Yishaans-MacBook-Air.local>
2026-02-20 19:03:40 +0530

    Add TranslationServiceProvider for trans() helper

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    Illuminate\Translation\TranslationServiceProvider::class,
     Illuminate\View\ViewServiceProvider::class,
     Illuminate\Auth\AuthServiceProvider::class,
     Illuminate\Database\DatabaseServiceProvider::class,
