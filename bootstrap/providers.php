2afe21c9b8dedec9261e2c5595dbd1ab56a9f35a
Yishaan <yishaan@Yishaans-MacBook-Air.local>
2026-02-20 18:34:40 +0530

    Fix view: add ViewServiceProvider first, add view config

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
+    Illuminate\View\ViewServiceProvider::class,
     Illuminate\Auth\AuthServiceProvider::class,
     Illuminate\Database\DatabaseServiceProvider::class,
-    Illuminate\View\ViewServiceProvider::class,
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
 ];
